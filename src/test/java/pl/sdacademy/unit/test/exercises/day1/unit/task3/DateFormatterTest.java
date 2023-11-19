a7f2f8a683331fd140e887daf4f4747f87c8943b
Pawel Dudko <paweldudko@outlook.com>
2023-11-19 13:17:01 +0100

    solution for day1_task3

---
+++ src/test/java/pl/sdacademy/unit/test/exercises/day1/unit/task3/DateFormatterTest.java
@@ -0,0 +1,29 @@
+package pl.sdacademy.unit.test.exercises.day1.unit.task3;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateFormatterTest {
+
+    /*
+    YYYY-MM-dd -> LocalDate
+    String w formacie YYYY-dd-MM
+     */
+    @Test
+    void shouldReturnCorrectDateAsString() {
+        //given
+        DateFormatter dateFormatter = new DateFormatter();
+        LocalDate input = LocalDate.of(1970, 1, 13);
+        String expectedResult = "1970-13-01";
+        //when
+        String result = dateFormatter.formatDate(input);
+        //then
+        assertEquals(expectedResult, result); //jUnit5
+        assertThat(result).isEqualTo(expectedResult); //assertJ
+    }
+
+}
