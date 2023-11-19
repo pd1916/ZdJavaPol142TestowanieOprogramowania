171b0ca4b0ad2c525cb25d79b75d33f2d7610c4c
Pawel Dudko <paweldudko@outlook.com>
2023-11-19 13:40:36 +0100

    solution for day1_task4

---
+++ src/test/java/pl/sdacademy/unit/test/exercises/day1/unit/task4/PersonTest.java
@@ -0,0 +1,41 @@
+package pl.sdacademy.unit.test.exercises.day1.unit.task4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    /*
+        age >= 0 && age <= 18
+
+        0-18
+        0
+        18
+        18+
+     */
+
+    @Test
+    void thatPersonIsAChild() {
+        //given
+        Person person = new Person("Jan", "Kowalski", 3);
+        //when
+        boolean result = person.isChild();
+        //then
+        assertTrue(result); //jUnit5
+        assertThat(result).isTrue(); //assertJ
+    }
+
+    @Test
+    void thatPersonIsNotAChild() {
+        //given
+        Person person = new Person("Jan", "Kowalski", 99);
+        //when
+        boolean result = person.isChild();
+        //then
+        assertFalse(result); //jUnit5
+        assertThat(result).isFalse(); //assertJ
+    }
+
+}
